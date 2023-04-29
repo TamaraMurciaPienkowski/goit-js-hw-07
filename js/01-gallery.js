8faa40239cc787c1ea48d4920ffebe06a326d955
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-04-29 12:48:55 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryPic = galleryItems
+  .map(
+    galleryItem => `<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    data-source="${galleryItem.original}"
+    alt="${galleryItem.description}"
+  />
+</a>
+</div>`
+  )
+  .join('');
 
+gallery.insertAdjacentHTML('afterbegin', galleryPic);
+const handleClick = e => {
+  e.preventDefault();
+  const bigImage = e.target.dataset.source;
+  basicLightbox.create(`<img src="${bigImage}">`).show();
+};
 console.log(galleryItems);
+gallery.addEventListener('click', handleClick);
