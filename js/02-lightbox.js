54f720159554fbfce85ec97f96cfd19d380bc2b9
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-04-30 11:46:56 +0200

    finished task 1 & 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryPic = galleryItems
+  .map(
+    galleryItem => `<li><a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a></li>`
+  )
+  .join('');
 
+gallery.insertAdjacentHTML('afterbegin', galleryPic);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
